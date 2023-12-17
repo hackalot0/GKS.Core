00505c4284ba5c5d85a78f66d35886933e3e55bb
hackalot0 <glive2b@gmail.com>
2023-12-17 15:14:18 +0100

    first code added

---
+++ GKS/GKS.Core/Types.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GKS.Core;
+
+public static class Types
+{
+    public static Type Object { get; } = typeof(object);
+    public static Type String { get; } = typeof(string);
+    public static Type Boolean { get; } = typeof(bool);
+    public static Type Byte { get; } = typeof(byte);
+    public static Type Int32 { get; } = typeof(int);
+}
